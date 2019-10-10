c10a360be41ba32adf2b8590e69a3778334457a7
Nicolás Pascual González <nicolaspascualgonzalez@gmail.com>
2019-10-10 16:14:44 +0200

    Added requirements.txt and retry library

--- crawler/marriott_reviews_crawler.py
+++ crawler/marriott_reviews_crawler.py
@@ -1,6 +1,6 @@
 from .selenium_facade import SeleniumFacade
 from .models import Response, Review
-
+from retry import retry
 
 class MarriotReviewsScraper(object):
     base_url = 'https://www.marriott.com/hotels/hotel-reviews/miaxr-the-st-regis-bal-harbour-resort/'
@@ -50,24 +50,28 @@ class MarriotReviewsScraper(object):
             for review in reviews
         ]
 
+    @retry(tries=3)
     def _parse_text(self, review):
         """
         Parses the text of the review 
         """
         return review.find_element_by_class_name('BVRRReviewText').text
 
+    @retry(tries=3)
     def _parse_title(self, review):
         """
         Parses the title of the review 
         """
         return review.find_element_by_class_name('BVRRReviewTitle').text
 
+    @retry(tries=3)
     def _parse_date(self, review):
         """
         Parses the date of the review 
         """
         return review.find_element_by_class_name('BVRRReviewDate').text
 
+    @retry(tries=3)
     def _parse_score(self, review):
         """
         Parses the score of the review and casts it.
@@ -79,6 +83,7 @@ class MarriotReviewsScraper(object):
             'div[itemprop=reviewRating] > span.BVRRRatingNumber'
         ).get_attribute('innerHTML').strip())
 
+    @retry(tries=3)
     def _parse_location_score(self, review):
         """
         Parses the location score of the revie and casts it.
@@ -99,12 +104,14 @@ class MarriotReviewsScraper(object):
         except ValueError:
             return None
 
+    @retry(tries=3)
     def _parse_author(self, review):
         """
         Parses the author of the review.
         """
         return review.find_element_by_class_name('BVRRUserNickname').text
 
+    @retry(tries=3)
     def _parse_responses(self, review):
         """
         Parses the responses of the review.
@@ -118,6 +125,7 @@ class MarriotReviewsScraper(object):
             for response in responses
         ]
 
+    @retry(tries=3)
     def _parse_response(self, response):
         """
         Parse each specific response.
